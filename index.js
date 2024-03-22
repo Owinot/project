e77fbf1ec45588b48a1a637d1c46b40541489f97
stephen owino <161324981+Owinot@users.noreply.github.com>
2024-03-22 12:49:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const currentUser="Bobby"
+const welcomeMessage=` Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
